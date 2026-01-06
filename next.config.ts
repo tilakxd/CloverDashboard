6c8025e66b2988f9fab9b463d71225a535a11c5f
Djkarmur <dipakkarmur45@gmail.com>
2026-01-06 04:45:02 -0700

    [CHANGE]: next js config changed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   basePath: '/clover/dashboard',
+  assetPrefix: '/clover/dashboard',
+  output: 'standalone',
 };
 
 export default nextConfig;
