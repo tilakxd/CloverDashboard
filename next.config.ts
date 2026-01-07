9b519dbe1dc58b84ad848093bc34615a0e675696
Djkarmur <dipakkarmur45@gmail.com>
2026-01-07 00:36:50 -0700

    [EXP]: root path and redirect path changed

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   // basePath and assetPrefix are for production deployment
   // Commented out for local development
-  // basePath: '/clover/dashboard',
-  // assetPrefix: '/clover/dashboard',
-  // output: 'standalone',
+  basePath: '/clover/dashboard',
+  assetPrefix: '/clover/dashboard',
+  output: 'standalone',
 };
 
 export default nextConfig;
